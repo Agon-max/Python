4c7c2ceee8011aa2851c1514723ed9b82b508bd1
Agon-Ramadani <agonagonramadani@gmail.com>
2025-04-08 19:12:27 +0200

    Create KlasatNePython.py

---
+++ KlasatNePython.py
@@ -0,0 +1,16 @@
+# Python eshte gjuhe programuese e
+# orientuar me pbjekte dhe klasa
+# Pra eshte object-oriented programming
+# Programimi permes klasave dhe objekteve mundeson qe kodi te shkruhet 
+# dhe te riperdoret (e qe te mos riperseritet)
+
+# Klasa
+# Ne Python, nje klae eshte i nje tipi i te dhenave i definuar nga nje perdorues
+# qe permban tre dhenat e tij dhe metodat qe perdoren per manipulim ne te.
+
+# Ne Python nje klase mund te krijohet duke perdorur termin "class",
+# shoqeruar me emrin e klases.
+#Sintaksa e nje klase eshte si ne vijim :
+ 
+# class Emri_i_klases:
+#     # Trupi i klases
